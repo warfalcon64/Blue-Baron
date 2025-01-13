4d6d633e2556ca41b9d7520fa8ded376cb61990d
warfalcon64 <peppythepig8@gmail.com>
2025-01-13 11:42:01 -0600

    missiles turn properly. Todo: implement missile damage, tune turn rates.

--- Assets/Scripts/Weapons/WeaponsAAMissile.cs
+++ Assets/Scripts/Weapons/WeaponsAAMissile.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class WeaponsAAMissile : WeaponsBase
 {
-    [Header("Missile Attributes")]
-    public float leadTimePercentage = 0.8f;
-    public  float maxTimePrediction = 2.0f;
-
     private float seekerFieldOfView = 1;
     private Vector2 aimPos;
     private Vector2 velocity;
@@ -33,26 +29,23 @@ public class WeaponsAAMissile : WeaponsBase
     private void PredictMovement()
     {
         Rigidbody2D targetRb = target.GetComponent<Rigidbody2D>();
-        float predictionTime = Mathf.Lerp(0, maxTimePrediction, leadTimePercentage);
-        aimPos = targetRb.position + targetRb.velocity * predictionTime;
+        float distance = Vector2.Distance(rb.position, targetRb.position);
+        float travelTime = distance / speed;
+        aimPos = targetRb.position + targetRb.velocity * travelTime;
     }
 
     private void TurnMissile()
     {
         Vector2 heading = aimPos - rb.position;
-
-        float angle = Mathf.Atan2(heading.y, heading.x) * Mathf.Rad2Deg;
-        float currentAngle = rb.rotation;
-        float newAngle = Mathf.MoveTowardsAngle(currentAngle, angle, turnSpeed * Time.deltaTime);
-        Debug.DrawLine(rb.position, aimPos);
-        
-        rb.MoveRotation(newAngle);
+        heading.Normalize();
+        float rotateAmount = Vector3.Cross(heading, transform.up).z;
+        rb.angularVelocity = -rotateAmount * turnSpeed;
     }
 
     public override void Setup(Vector2 shootDirection, Vector2 shipVelocity, ShipBase source)
     {
         relativeVelocity = shipVelocity;
-        Destroy(gameObject, lifetime);
+        //Destroy(gameObject, lifetime);
     }
 
     //private Vector2 GetTargetLeadingPosition(Vector2 targetAcceleration, int iterations)
