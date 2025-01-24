e368ebdf468a508c55aabebc47947b48f370fbd3
warfalcon64 <peppythepig8@gmail.com>
2025-01-24 00:56:05 -0600

    resized radar so parent GO has scale of 1, and made it larger overall. Added radar blip. Todo: Figure out proper conversion for radar sweep height to world units to set length of raycast correctly. Then, make radar blips appear and fade when ship is hit.

--- Assets/Scripts/UI/Battle/Radar.cs
+++ Assets/Scripts/UI/Battle/Radar.cs
@@ -10,11 +10,19 @@ public class Radar : MonoBehaviour
     public float rotateSpeed = 100f;
     public float radarDistance = 1776f;
 
-    private float currentAngle;
+    private List<Collider2D> colliders;
+
+    private void Awake()
+    {
+        colliders = new List<Collider2D>();
+        radarDistance = radarSweep.sizeDelta.x;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         // *** FOR LARGER SHIP CLASSES IN FUTURE, MAY HAVE TO SCALE UP RADAR!
+        print(radarDistance);
     }
 
     // Update is called once per frame
@@ -23,16 +31,22 @@ public class Radar : MonoBehaviour
         radarSweep.eulerAngles -= new Vector3(0, 0, rotateSpeed * Time.deltaTime);
 
         RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, GetVectorFromAngle(radarSweep.eulerAngles.z), radarDistance);
-
+        Debug.DrawRay(transform.position, GetVectorFromAngle(radarSweep.eulerAngles.z) * radarDistance, Color.red);
+        
+        // * Hardcoded tag comparison, may need to remove in future
         if (raycastHit.collider != null && !raycastHit.collider.CompareTag("Blue") && raycastHit.collider.GetComponent<ShipBase>())
         {
-            print(raycastHit.collider.gameObject);
+            if (!colliders.Contains(raycastHit.collider))
+            {
+                colliders.Add(raycastHit.collider);
+                print(raycastHit.collider.gameObject);
+            }
         }
     }
 
     private Vector3 GetVectorFromAngle(float angle)
     {
-        float angleRad = angle * (Mathf.PI / 180f);
+        float angleRad = (angle + 90) * (Mathf.PI / 180f);
         return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
     }
 }
